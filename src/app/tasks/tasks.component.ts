8fb5d15b891799aa0cc1c3f33f804789e8101393
Vishal Sachdeva <vishal@Vishals-MacBook-Air.local>
2018-08-23 16:38:43 -0400

    changed logic for dropdown binding

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Task } from '../models/task';
 import { TaskService } from '../services/task.service';
 
+
 @Component({
   selector: 'my-app',
   templateUrl: './tasks.component.html',
@@ -9,7 +10,6 @@ import { TaskService } from '../services/task.service';
 export class TasksComponent implements OnInit  {
   tasks: Task[];
   task: Task;
-  priority: string;
   priorityList = [
     { name: "High", id: 1 },
     { name: "Medium", id: 2 },
@@ -22,10 +22,8 @@ export class TasksComponent implements OnInit  {
     this.tasks = this.taskService.getTasks();
   }
   addTask():void{
-    this.task.priority = this.priorityList.find(e => e.name === this.priority).id;
     this.task.id = this.getRandomId();
     this.tasks = this.taskService.addTask(this.task);
-    this.priority = '';
     this.task = new Task();
   }
   removeTask(id: number): void{
